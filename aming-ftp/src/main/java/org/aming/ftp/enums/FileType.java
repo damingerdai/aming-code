3c6f1d0f38ccd1a422c80f134c3c23763f15a3df
Arthur Ming <937765518@qq.com>
2018-01-02 22:24:03 +0800

    add get method form file type int

--- aming-ftp/src/main/java/org/aming/ftp/enums/FileType.java
+++ aming-ftp/src/main/java/org/aming/ftp/enums/FileType.java
@@ -1,12 +1,15 @@
 package org.aming.ftp.enums;
 
+import org.aming.ftp.exceptions.FtpException;
+import org.aming.ftp.exceptions.FtpExceptionBuilder;
+
 /**
  * @author daming
  * @version 2018/1/2.
  */
 public enum FileType {
 
-    ASCII_FILE_TYPE(0),EBCDIC_FILE_TYPE(1),BINARY_FILE_TYPE(3),LOCAL_FILE_TYPE(4);
+    ASCII_FILE_TYPE(0), EBCDIC_FILE_TYPE(1), BINARY_FILE_TYPE(3), LOCAL_FILE_TYPE(4);
 
 
     private int type;
@@ -16,6 +19,15 @@ public enum FileType {
 
     FileType(int type) {
         this.type = type;
+    }
+
+    public static FileType getFileType(int type) throws FtpException{
+        for (FileType ft : values()) {
+            if (ft.type == type) {
+                return ft;
+            }
+        }
+        throw FtpExceptionBuilder.buildFtpException("file type '{0}' is invalid", type);
     }
 
 }

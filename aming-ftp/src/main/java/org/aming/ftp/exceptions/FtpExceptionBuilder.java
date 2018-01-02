3fa744d6a79a98455bdf46bbb33dc7d9b02268df
Arthur Ming <937765518@qq.com>
2018-01-02 22:24:27 +0800

    get exception for format

--- aming-ftp/src/main/java/org/aming/ftp/exceptions/FtpExceptionBuilder.java
+++ aming-ftp/src/main/java/org/aming/ftp/exceptions/FtpExceptionBuilder.java
@@ -1,5 +1,7 @@
 package org.aming.ftp.exceptions;
 
+import java.text.MessageFormat;
+
 /**
  * @author daming
  * @version 2018/1/2.
@@ -8,6 +10,10 @@ public class FtpExceptionBuilder {
 
     public static FtpException buildFtpException(String message, Throwable cause) {
         return new FtpException(message, cause);
+    }
+
+    public static FtpException buildFtpException(String pattern, Object...arguments) {
+        return new FtpException(MessageFormat.format(pattern, arguments));
     }
 
     public static FtpException buildFtpException(String message) {

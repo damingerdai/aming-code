81323a2122ac8ef1f74716e569edd7769d65877d
Arthur.Ming <arthur.ming@acxiom.com>
2018-01-05 17:48:19 +0800

    fix bug

--- aming-ftp/src/main/java/org/aming/ftp/pools/FtpClientFactory.java
+++ aming-ftp/src/main/java/org/aming/ftp/pools/FtpClientFactory.java
@@ -1,22 +1,22 @@
 package org.aming.ftp.pools;
 
+import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.pool2.BasePooledObjectFactory;
 import org.apache.commons.pool2.PooledObject;
-import sun.net.ftp.FtpClient;
 
 /**
  * @author daming
  * @version 2017/12/28.
  */
-public class FtpClientFactory extends BasePooledObjectFactory<FtpClient> {
+public class FtpClientFactory extends BasePooledObjectFactory<FTPClient> {
 
     @Override
-    public FtpClient create() throws Exception {
+    public FTPClient create() throws Exception {
         return null;
     }
 
     @Override
-    public PooledObject<FtpClient> wrap(FtpClient obj) {
+    public PooledObject<FTPClient> wrap(FTPClient obj) {
         return null;
     }
 }
